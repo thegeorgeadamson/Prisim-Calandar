f9bd3eabbd3bbd5b05d54881b8d5a9eacd19e075
George Adamson <georgehunteradamson@gmail.com>
2025-09-29 17:55:25 +1300

    Initial Commit

---
+++ Prisim Calandar/Prisim_CalandarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prisim_CalandarApp.swift
+//  Prisim Calandar
+//
+//  Created by George Adamson on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Prisim_CalandarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
